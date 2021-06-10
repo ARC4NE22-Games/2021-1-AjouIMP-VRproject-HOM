3e545fdf3119e36a7ffbff80b4317b4a973cd4a1
hong3883 <hong3883@naver.com>
2021-06-11 02:05:03 +0900

    Fix Bug
    Fix Bug

--- HouseOfMadness/Assets/Scripts/MainMenu.cs
+++ HouseOfMadness/Assets/Scripts/MainMenu.cs
@@ -1,10 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject light;
     private AudioSource _audioSource;
 
     void Start()
@@ -12,6 +13,18 @@ public class MainMenu : MonoBehaviour
         _audioSource = GetComponent<AudioSource>();
         _audioSource.Play();
     }
+
+    private void Update()
+    {
+        RotateLight();
+    }
+
+    void RotateLight()
+    {
+        light.transform.Rotate(new Vector3(-0.015f, -0.05f, 0));
+
+    }
+
     public void Start_Menu()
     {
         SceneManager.LoadScene("GameScene");
