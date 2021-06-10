cdf5a3dfbc40f5fc89b649fcee81da2045e826cd
hong3883 <hong3883@naver.com>
2021-06-10 18:12:21 +0900

    Add Zombie1 & 2 AI (Pathfinding
    Add Zombie1 & 2 AI (Pathfinding

---
+++ HouseOfMadness/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private int _health, _bullet;
+    
+    void Init()
+    {
+        _health = 5;
+        _bullet = 0;
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void Attacked()
+    {
+        if (_health > 0)
+        {
+            _health--;
+            Debug.Log(_health);
+        }
+        else
+        {
+            // game over
+        }
+    }
+}
